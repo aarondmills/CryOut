a055c9675cf479c113cc1f9e0c598cefb4ee4ebf
Aaron Mills <aaron@jailbreakcreative.com>
2011-04-21 13:22:16 -0400

    Changed the admin to include the names of coordinators on the county pages, including their city and state

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,4 +1,9 @@
 class Coordinator < ActiveRecord::Base
 	belongs_to :county
 	belongs_to :state
+
+	def name
+		fullname = self.firstname + " " + self.lastname + " from " + self.city + " " + self.state.abreviation
+		return fullname
+	end
 end
