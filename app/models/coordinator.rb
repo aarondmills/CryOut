7135189768b6822a22b66b3cb57a9015073c30c4
Aaron Mills <aaron@jailbreakcreative.com>
2011-04-25 12:03:29 -0400

    issue with admin

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -2,5 +2,8 @@ class Coordinator < ActiveRecord::Base
 	belongs_to :county
 	belongs_to :state
 
-	
+	def name
+		fullname = self.firstname + " " + self.lastname + " from " + self.city
+		return fullname
+	end
 end
