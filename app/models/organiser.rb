eea9ac4016aa129a25a953d91ad9cc14953991ad
Aaron Mills <aaron@jailbreakcreative.com>
2011-05-19 23:45:38 -0400

    added state organisers

--- app/models/organiser.rb
+++ app/models/organiser.rb
@@ -1,2 +1,3 @@
 class Organiser < ActiveRecord::Base
+	belongs_to :state
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,7 @@
 class State < ActiveRecord::Base
 	has_many :counties
 	has_many :coordinators
+	has_one :organiser
 
-	accepts_nested_attributes_for :counties, :coordinators
+	accepts_nested_attributes_for :counties, :coordinators, :organiser
 end
